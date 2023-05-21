6b3f56c17eb724d57708793edb0f258d9d3d541f
AleksandrLiagushin <a.lyagushin@yandex.ru>
2023-05-21 15:13:33 +0530

    <refactor> changeEpicStatus moved to TaskManager

--- src/data/storage/EpicTasksList.java
+++ src/data/storage/EpicTasksList.java
@@ -1,9 +1,7 @@
 package data.storage;
 
 import data.types.EpicTask;
-import data.types.TaskStatus;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Set;
@@ -33,21 +31,8 @@ public class EpicTasksList {
         this.epicTasksList.clear();
     }
 
-    public void changeEpicStatus(int epicID, SubTasksList subTasksList) {
-        ArrayList<Integer> tasksByEpic = getTaskByID(epicID).getSubTasksList();
-        EpicTask epic = getTaskByID(epicID);
-        TaskStatus status = TaskStatus.NEW;
-        if (!tasksByEpic.isEmpty()) {
-            status = subTasksList.getTaskByID(tasksByEpic.get(0)).getStatus();
-            for (Integer integer : tasksByEpic) {
-                if (!status.equals(subTasksList.getTaskByID(integer).getStatus())) {
-                    status = TaskStatus.IN_PROGRESS;
-                    break;
-                }
-            }
-        }
-        epic.setStatus(status);
-        addEpicTaskToList(epicID, epic);
+    public HashMap<Integer, EpicTask> getEpicTasksList() {
+        return epicTasksList;
     }
 
     @Override
@@ -59,7 +44,7 @@ public class EpicTasksList {
             out.append("TaskID='").append(id).append("' Task{")
                     .append(Arrays.toString(new EpicTask[]{epic})).append("}")
                     .append("SubTasksIDs=")
-                    .append(epic.getSubTasksList()).append("\n");
+                    .append(epic.getSubTasksIDsList()).append("\n");
         }
         return out.toString();
     }
