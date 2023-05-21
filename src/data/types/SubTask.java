dd162cdcd6ced97aa1ceb9b1a900e31ff08e6166
AleksandrLiagushin <a.lyagushin@yandex.ru>
2023-05-21 15:12:25 +0530

    <refactor> added class constructor

--- src/data/types/SubTask.java
+++ src/data/types/SubTask.java
@@ -3,6 +3,11 @@ package data.types;
 public class SubTask extends Task {
     private int ownedByEpic;
 
+    public SubTask(String taskName, String description, TaskStatus status, int ownedByEpic) {
+        super(taskName, description, status);
+        this.ownedByEpic = ownedByEpic;
+    }
+
     public int getOwnedByEpic() {
         return ownedByEpic;
     }
@@ -11,12 +16,4 @@ public class SubTask extends Task {
         this.ownedByEpic = epicID;
     }
 
-    public static SubTask toSubTask (Task task) {
-        SubTask sub = new SubTask();
-        sub.setTaskName(task.getTaskName());
-        sub.setDescription(task.getDescription());
-        sub.setStatus(task.getStatus());
-        sub.setCreationDate();
-        return sub;
-    }
 }
