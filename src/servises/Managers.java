e079a9f03142514c7858f68acf394c8abf0da3cb
AleksandrLiagushin <a.lyagushin@yandex.ru>
2023-05-28 15:27:45 +0530

    feat: util class for calling managers

--- src/servises/Managers.java
+++ src/servises/Managers.java
@@ -1,4 +1,14 @@
 package servises;
 
 public class Managers {
+
+    private Managers(){}
+
+    public static HistoryManager getDefaultHistoryManager() {
+        return new InMemoryHistoryManager();
+    }
+
+    public static TaskManager getDefaultTaskManager() {
+        return new InMemoryTaskManager();
+    }
 }
