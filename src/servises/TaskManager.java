2d2f640d8c227919a79647c3f7127b9f1b82c9b2
AleksandrLiagushin <a.lyagushin@yandex.ru>
2023-05-28 15:32:05 +0530

    refactor: rebuilt to generic class

--- src/servises/TaskManager.java
+++ src/servises/TaskManager.java
@@ -8,33 +8,33 @@ import java.util.ArrayList;
 
 public interface TaskManager {
 
-    public void createNewTask(Task task);
-    public void createNewEpic(EpicTask epic);
-    public void createSubTask(SubTask sub);
+    void createNewTask(Task task);
+    void createNewEpic(EpicTask epic);
+    void createSubTask(SubTask sub);
 
-    public Task findTaskByID(int taskID);
-    public EpicTask findEpicByID(int epicID);
-    public SubTask findSubTaskByID(int subID);
+    Task findTaskByID(int taskID);
+    EpicTask findEpicByID(int epicID);
+    SubTask findSubTaskByID(int subID);
 
-    public ArrayList<Task> getAllTasks();
-    public ArrayList<SubTask> getAllSubTasks();
-    public ArrayList<EpicTask> getAllEpicTasks();
+    ArrayList<Task> getAllTasks();
+    ArrayList<SubTask> getAllSubTasks();
+    ArrayList<EpicTask> getAllEpicTasks();
 
-    public void refreshTask(Task task);
-    public void refreshEpicTask(EpicTask epic);
-    public void refreshSubTask(SubTask sub);
+    void refreshTask(Task task);
+    void refreshEpicTask(EpicTask epic);
+    void refreshSubTask(SubTask sub);
 
-    public void deleteTaskByID(int taskID);
-    public void deleteEpicTaskByID(int epicID);
-    public void deleteSubTaskByID(int subID);
+    void deleteTaskByID(int taskID);
+    void deleteEpicTaskByID(int epicID);
+    void deleteSubTaskByID(int subID);
 
-    public void deleteAllTasks();
-    public void deleteAllEpics();
-    public void deleteAllSubs();
+    void deleteAllTasks();
+    void deleteAllEpics();
+    void deleteAllSubs();
 
-    public ArrayList<SubTask> getSubTasksListOfEpicByID(int epicID);
+    ArrayList<SubTask> getSubTasksListOfEpicByID(int epicID);
 
-    public void changeEpicStatus(int epicID);
+    void changeEpicStatus(int epicID);
 
-    public void generateTaskID();
+    ArrayList<Task> getHistory();
 }
