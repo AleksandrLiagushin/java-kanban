aef39d595e165ab90d85116a20d6172985d6a85c
AleksandrLiagushin <a.lyagushin@yandex.ru>
2023-05-29 21:55:01 +0530

    style: formatted

--- src/servises/HistoryManager.java
+++ src/servises/HistoryManager.java
@@ -2,11 +2,11 @@ package servises;
 
 import data.types.Task;
 
-import java.util.ArrayList;
+import java.util.List;
 
-    public interface HistoryManager {
+public interface HistoryManager {
 
-        void add(Task task);
+    void add(Task task);
 
-        ArrayList<Task> getHistory();
+    List<Task> getHistory();
 }
