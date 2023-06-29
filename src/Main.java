affca1eb0e407c5a5bb55a6aff756d372f4b9430
AleksandrLiagushin <a.lyagushin@yandex.ru>
2023-06-29 23:36:47 +0530

    refactor: rebuilt according changes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import ru.yandex.practicum.kanban.model.Epic;
+import ru.yandex.practicum.kanban.model.Subtask;
+import ru.yandex.practicum.kanban.model.Task;
+import ru.yandex.practicum.kanban.model.TaskStatus;
 import ru.yandex.practicum.kanban.service.FileBackedTaskManager;
 
 public class Main {
@@ -12,5 +16,36 @@ public class Main {
         System.out.println(taskManager.getAllSubtasks());
         System.out.println("******************************");
         System.out.println(taskManager.getHistory());
+
+        taskManager.createTask(new Task.TaskBuilder("task1123")
+                .withDescription("task2")
+                .withStatus(TaskStatus.NEW)
+                .build());
+        taskManager.createSubtask(new Subtask.SubtaskBuilder("Задача 1", TaskStatus.NEW, 13)
+                .withDescription("sub2")
+                .build());
+        taskManager.createSubtask(new Subtask.SubtaskBuilder("Задача 1", TaskStatus.DONE, 13)
+                .withDescription("sub1")
+                .build());
+        taskManager.createSubtask(new Subtask.SubtaskBuilder("Задача 1", TaskStatus.NEW, 14)
+                .withDescription("sub2")
+                .build());
+        taskManager.createSubtask(new Subtask.SubtaskBuilder("Задача 1", TaskStatus.NEW, 14)
+                .withDescription("sub2")
+                .build());
+        taskManager.createEpic(new Epic.EpicBuilder("Важный эпик 2")
+                .withDescription("epic1")
+                .build());
+        taskManager.findTaskById(24);
+        taskManager.findEpicById(31);
+        taskManager.findSubtaskById(30);
+        taskManager.findTaskById(24);
+        taskManager.findEpicById(31);
+        taskManager.findSubtaskById(30);
+        taskManager.findTaskById(24);
+        taskManager.findEpicById(31);
+        taskManager.findSubtaskById(30);
+        taskManager.findTaskById(26);
+        taskManager.findTaskById(25);
     }
 }
